a69ce1b28b01a3eb5ffe952fe8ed594e8cb9bf8b
nguyenminhnhut <nguyenminhnhut>
2024-07-23 15:41:29 +0700

    dark mode

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from "@/components/ui/model-toggle";
 export default function Home() {
   return (
     <div>
       <UserButton
        afterSwitchSessionUrl="/"
       />
+      <ModeToggle/>
     </div>
   );
 }
--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 const font = Open_Sans({ subsets: ["latin"] });
 import { viVN } from "@clerk/localizations";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "404Connect",
@@ -17,8 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider localization={viVN}>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressContentEditableWarning>
+        <body className={cn(font.className,"bg-white dark:bg-[#313338]")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="discord-theme"
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
